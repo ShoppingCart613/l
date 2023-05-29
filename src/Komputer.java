ff5b194e5ed9a594c6c6ad3a1af744febb5078da
szymon.sz <szymon366@vp.pl>
2023-05-29 12:35:23 +0200

    initial commit

--- src/Komputer.java
+++ src/Komputer.java
@@ -1,6 +1,25 @@
 public class Komputer {
     private int numer;
-    private boolean czydziala = true;
+    private boolean czydziala;
 
+    public Komputer(int numer, boolean czydziala) {
+        this.numer = numer;
+        this.czydziala = czydziala;
+    }
 
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public boolean isCzydziala() {
+        return czydziala;
+    }
+
+    public void setCzydziala(boolean czydziala) {
+        this.czydziala = czydziala;
+    }
 }
